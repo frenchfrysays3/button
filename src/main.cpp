7e2d4050a7313bd9938b02cdf99f3b64a452cd0f
Lucas Fry <lucas.e.fry@outlook.com>
2025-06-28 18:36:20 -0500

    Fixed typo

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,20 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/Man
+#include <Geode/modify/MenuLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MenuLayer, MyMenuLayer) {
+	bool init() {
+		// start up the class
+
+		// This is used when we are appending to the init function (which we are)
+		if (!MenuLayer::init()) {
+			return false;
+		}
+
+		log::info("Starting up...");
+
+		// make the button
+
+	}
+}
