e6e7c68ef9ccb7792e007bceeb1516c8f7e964e1
Lucas Fry <lucas.e.fry@outlook.com>
2025-06-28 19:00:33 -0500

    Finished up init() function and added onBtn(CCObject*) function

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,35 @@ class $modify(MyMenuLayer, MenuLayer) {
 		log::info("Starting up...");
 
 		// make the button
+		auto spr = ButtonSprite::create("Click me for SE.");
+		auto btn = CCMenuItemSpriteExtra::create(spr, this, menu_selector(MyMenuLayer::onBtn));
 
+		// declare the menu and add achild to it
+		auto menu = CCMenu::create();
+		menu->addChild(btn);
+
+		// update menu
+		menu->updateLayout();
+
+		// finish up
+		return true;
+	}
+
+	// Function(onBtn) for button(btn)
+	void onBtn(CCObject*) {
+		// Show a popup
+		FLAlertLayer::create(
+			"Button", // name
+			"I have played a sound effect!", // body
+			"Ok cool" // button
+		)->show();
+
+		// Function for sound effects
+		soundEffect();
+	}
+
+	// function for sound effect (soundEffect)
+	void soundEffect() {
+		
 	}
 };
